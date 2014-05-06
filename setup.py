3dce47ee86c7c8a385e10c4369177ba56deae2eb
Yoan Arnaudov <jonidev@gmail.com>
2014-05-06 23:38:50 +0300

    version update

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'url_extract',
     packages = ['url_extract'],
-    version = '0.1',
+    version = '0.14',
     description = 'extracts tld, domain, subdomains and query from URLs. It also validates the URLs.',
     author = 'Yoan Arnaudov',
     author_email = 'jonidev@gmail.com',
     url = 'https://github.com/nacholibre/url_extract',
-    download_url = 'https://github.com/nacholibre/url_extract/tarball/0.1',
+    download_url = 'https://github.com/nacholibre/url_extract/tarball/0.14',
     keywords = ['domain', 'tld', 'extract'],
     classifiers = [],
 )
